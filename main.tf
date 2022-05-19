f508016eb8e57b9140436aa7f593964283026997
arlandleo <arleon663@gmail.com>
2022-05-19 19:59:51 -0400

    creating a bucket

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucke" "b" {
+  bucket = "arland2020-my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
